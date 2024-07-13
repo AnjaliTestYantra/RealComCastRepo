1d637a86658a6041fc3d31d76721d6dc075e766f
Sneha <Sneha@TYSS-Sneha>
2024-07-13 09:49:28 +0530

    Sending pull request

---
+++ VTIGER_CRM_ATE/src/test/java/demo/Pull_Req.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Pull_Req {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Forked and trying to pull");
+	}
+
+}
